9df946e4725e3b124cc6795624a5777ba26ce065
Shidiq <shidiqbachtiar05@gmail.com>
2026-04-05 16:44:52 +0700

    menambahkan class SayaMusicTrack

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TP_MODUL6_103022400044;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayaMusicTrack track = new SayaMusicTrack("Lagu Favorit");
+
+        track.IncreasePlayCount(10);
+        track.PrintTrackDetails();
+    }
+}
---
+++ SayaMusicTrack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_MODUL6_103022400044
+{
+
+    public class SayaMusicTrack
+    {
+        private int id;
+        private int playCount;
+        private string title;
+
+        public SayaMusicTrack(string title)
+        {
+            Random rand = new Random();
+            this.id = rand.Next(1000000, 9999999);
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            playCount += count;
+        }
+
+        public void PrintTrackDetails()
+        {
+            Console.WriteLine($"ID: {id}");
+            Console.WriteLine($"Title: {title}");
+            Console.WriteLine($"Play Count: {playCount}");
+        }
+    }
+}
